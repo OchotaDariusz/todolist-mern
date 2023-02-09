7960ed6f831182c7808fb340268f0eb05203a7b2
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-02-09 06:35:24 +0100

    Better UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,29 @@
-import React, {useEffect, useState} from 'react'
-import Header from './components/Header/Header'
-import TodoList from './components/TodoList/TodoList'
-import AddTodoButton from "./components/AddTodoButton/AddTodoButton"
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
+import Card from "./components/UI/Card";
+import AddTodoButton from "./components/AddTodoButton/AddTodoButton";
 import { getTodos } from "./utils/utils";
-import './App.css'
-
+import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
 
-    const [todoList, setTodoList] = useState([])
+  useEffect(() => {
+    getTodos(setTodoList)
+      .then(() => console.log("Data fetched."))
+      .catch((err) => console.error(err.message));
+  }, []);
 
-    useEffect(() => {
-        getTodos(setTodoList).then(() => console.log('Data fetched.'))
-            .catch(err => console.error(err.message))
-    }, [])
-
-    return (
-        <div className="App">
-            <Header />
-            <AddTodoButton setTodoList={setTodoList} />
-            <TodoList todoList={todoList} setTodoList={setTodoList} />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <AddTodoButton setTodoList={setTodoList} />
+      <Card>
+        <TodoList todoList={todoList} setTodoList={setTodoList} />
+      </Card>
+    </div>
+  );
 }
 
-export default App
+export default App;
