1ea97961a6e6276690a323890cf9495830e8f262
Ochota Dariusz <ochota.dariusz@gmail.com>
2023-02-17 10:40:02 +0100

    style: format code

--- frontend/src/components/TodoList/TodoList.js
+++ frontend/src/components/TodoList/TodoList.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import TodoElement from "../TodoElement/TodoElement"
+import React from "react";
+import TodoElement from "../TodoElement/TodoElement";
 
 export default function TodoList({ todoList, setTodoList }) {
-
-    return (
-        <div>
-            { todoList.map(task => <TodoElement
-                setTodoList={setTodoList}
-                taskName={task.title}
-                key={task._id}
-                taskID={task._id}
-            />) }
-        </div>
-    )
+  return (
+    <div>
+      {todoList.map((task) => (
+        <TodoElement
+          setTodoList={setTodoList}
+          taskName={task.title}
+          key={task._id}
+          taskID={task._id}
+        />
+      ))}
+    </div>
+  );
 }
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,9 +1,12 @@
 export const getTodos = async (setCallback) => {
-    const todos = await fetch('https://todolist-mern-app.onrender.com/api/v1/todos', {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    const fetchedTodos = await todos.json()
-    setCallback(fetchedTodos.data)
-}
+  const todos = await fetch(
+    "https://todolist-mern-app.onrender.com/api/v1/todos",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const fetchedTodos = await todos.json();
+  setCallback(fetchedTodos.data);
+};
